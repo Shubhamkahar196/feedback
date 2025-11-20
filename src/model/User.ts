dd9e1c5d875692a82351097a0c6f3d6449e9a7d3
Shubham kahar <Shubhamkahar196@gmail.com>
2025-11-20 15:53:06 +0530

    Added  models

---
+++ src/model/User.ts
@@ -0,0 +1,76 @@
+import mongoose, {Schema,Document} from 'mongoose';
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    
+        createdAt: {
+            type:Date,
+            required: true,
+            default: Date.now
+        }
+    
+})
+
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpiry: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[]
+}
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: [true, "Username is required"]
+    },
+    email:{
+        type: String,
+        required: [true,'Email is required'],
+        unique: true,
+         match: [/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/, 'please use a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "VerifyCode is required"]
+    },
+    verifyCodeExpiry: {
+        type: Date,
+        required: [true, "Verify Code Expiry is required"]
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages:[MessageSchema]
+
+})
+
+
+// exporting - first check model have or not if not have model then create
+
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema) 
+export default UserModel;
+
+
+
