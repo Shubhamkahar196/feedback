3608207f34c38f204d5378ed462c4933c3261607
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-03 11:38:17 +0530

    added verify-code

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,6 +1,56 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
 
+
 export async function POST(request: Request){
     await dbConnect();
+
+    try {
+        const {username,code} = await request.json()
+
+       const decodedUsername =  decodeURIComponent(username)
+       const user = await UserModel.findOne({username: decodedUsername})
+       if(!user){
+        return Response.json({
+            success: false,
+            message: "User not found"
+        },
+    {status: 500})
+       }
+
+    //    checking code
+
+    const isCodeValid = user.verifyCode === code
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+
+    if(isCodeValid && isCodeNotExpired){
+        user.isVerified = true
+        await user.save()
+    }else if(!isCodeNotExpired){
+        return Response.json({
+            success: false,
+            message: "Verification code has expired. please signup again to get a new code"
+        },{status:400})
+    }else {
+        return Response.json({
+            success: false,
+            message: "Incorrect Verification Code"
+        },{status: 400})
+    }
+
+    return Response.json({
+        success: true,
+        message: 'Account verified successfully'
+
+    },{status:200})
+
+    } catch (error) {
+        console.error("Error verifying user", error)
+        return Response.json(
+            {
+                success: false,
+                message: "Error verifying user"
+            }
+        )
+    }
 }
