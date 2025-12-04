8659ad73c6489f44d6054c94fa6619f85f6241a1
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-04 15:13:25 +0530

    Added get method for accepting message

--- src/app/api/accept-message/route.ts
+++ src/app/api/accept-message/route.ts
@@ -40,12 +40,62 @@ export async function POST(request: Request) {
     }
 
     return Response.json({
-        success: true,
-        message: "User status updated successfully",
-        updatedUser
-    })
+      success: true,
+      message: "User status updated successfully",
+      updatedUser,
+    });
   } catch (error) {
-    console.log("Failed to update user status to accept messages",error);
+    console.log("Failed to update user status to accept messages", error);
+    return Response.json(
+      {
+        success: false,
+        message: "failed to update user status to accept messages",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+
+  try {
+    const foundUser = await UserModel.findById(userId);
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "User found successfully",
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Failed to update user status to accept messages", error);
     return Response.json(
       {
         success: false,
