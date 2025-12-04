05327b655f0de268c1afbae8020eaffbd5d3a792
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-04 14:55:16 +0530

    added accepting-message : POST request

---
+++ src/app/api/accept-message/route.ts
@@ -0,0 +1,57 @@
+import { getServerSession, User } from "next-auth";
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { authOptions } from "../auth/[...nextauth]/options";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update user status to accept messages",
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json({
+        success: true,
+        message: "User status updated successfully",
+        updatedUser
+    })
+  } catch (error) {
+    console.log("Failed to update user status to accept messages",error);
+    return Response.json(
+      {
+        success: false,
+        message: "failed to update user status to accept messages",
+      },
+      { status: 500 }
+    );
+  }
+}
