9cc4a3fa20685a87bdc161fad09285f22816f8dd
Shubham kahar <Shubhamkahar196@gmail.com>
2025-12-04 16:18:38 +0530

    fixed typescript issue

--- src/app/api/suggest-message/route.ts
+++ src/app/api/suggest-message/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: Request) {
       },
     });
 
-    return result.toAIStreamResponse();
+    return result.toTextStreamResponse();
   } catch (error: any) {
     console.error("Gemini API Error:", error);
 
@@ -48,39 +48,3 @@ export async function POST(req: Request) {
 }
 
 
-
-// import OpenAI from 'openai';
-// import {OpenAIStream, StreamingTextResponse} from 'ai';
-// import { NextResponse } from 'next/server';
-
-// const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY.
-// })
-
-// export const runtime = 'edge';
-
-// export async function POST(req:Request){
-//     try {
-
-//         const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be?|| What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment"
-
-//         const response = await openai.chat.completion.create({
-//             model: 'gpt-3.5-turbo',
-//             stream: true,
-//             messages,
-//         })
-
-//         const stream = OpenAIStream(response);
-//         return new StreamingTextResponse(stream)
-//     } catch (error) {
-//         if(error instanceof OpenAI.APIError){
-//          const {name, status,headers,message} = error;
-//          return NextResponse.json({
-//             name,status,headers,message
-//          },status)
-//         }else{
-//             console.error('An unexpected error occured')
-//             throw error
-//         }
-//     }
-// }
